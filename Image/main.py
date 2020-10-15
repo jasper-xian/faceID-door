85d6896bcd30e7904012510c5667fec089d5468c
bhrotovy <bhrotovy@uwaterloo.ca>
2020-10-15 13:02:07 -0400

    made some polishing changes

--- Image/main.py
+++ Image/main.py
@@ -1,8 +1,10 @@
 import face_recognition
 import os
 import time
+import shutil
 
 known_faces = []
+
 known_files = os.listdir(os.getcwd()+'/Known')
 
 for image in known_files:
@@ -10,11 +12,23 @@ for image in known_files:
     known_faces.append(face_recognition.face_encodings(face)[0])
 
 while True:
-    # insert code to fetch uknown image from camera
+    #add new faces from database to new folder
 
-    # processes unknown image then removes it
+    # process new faces, then add them to Known folder
+    known_files = os.listdir(os.getcwd() + '/New Faces')
+
+    for image in known_files:
+        face = face_recognition.load_image_file(os.getcwd() + '/New Faces/' + image)
+        known_faces.append(face_recognition.face_encodings(face)[0])
+        shutil.move(os.getcwd() + '/New Faces/' + image, os.getcwd()+'/Known/'+image)
+
+    # insert code to fetch unknown image from camera
+
+
+    # processes image then changes removes it
     unknown = face_recognition.load_image_file("unknown.jpg")
-    # os.remove("unknown.jpg")
+    os.remove("unknown.jpg")
+    shutil.copyfile("placeholder.jpg","unknown.jpg")
 
     unknown_encodings = face_recognition.face_encodings(unknown)
 
@@ -26,7 +40,7 @@ while True:
                 isKnown = True
 
     if isKnown:
-        print("Known -> door is opened")
+        print("\nKnown -> door is opened")
         time.sleep(0.5)
         print("waiting...")
         time.sleep(9.5)

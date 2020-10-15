5ec6db4d3e7288a9f59744dc31cc9ea703185b01
bhrotovy <bhrotovy@uwaterloo.ca>
2020-10-15 12:15:30 -0400

    Added loop that constantly checks if picture in question (which will be pulled from a link) is known or unknown

--- Image/main.py
+++ Image/main.py
@@ -8,22 +8,23 @@ for image in known_files:
     face = face_recognition.load_image_file(os.getcwd()+'/Known/'+image)
     known_faces.append(face_recognition.face_encodings(face)[0])
 
-# insert code to fetch uknown image from camera
+while True:
+    # insert code to fetch uknown image from camera
 
-# processes unknown image then removes it
-unknown = face_recognition.load_image_file("unknown.jpg")
-# os.remove("unknown.jpg")
+    # processes unknown image then removes it
+    unknown = face_recognition.load_image_file("unknown.jpg")
+    # os.remove("unknown.jpg")
 
-unknown_encodings = face_recognition.face_encodings(unknown)
+    unknown_encodings = face_recognition.face_encodings(unknown)
 
-isKnown = False
-for unknown_encoding in unknown_encodings:
-    results = face_recognition.compare_faces(known_faces, unknown_encoding)
-    for result in results:
-        if result:
-            isKnown = True
+    isKnown = False
+    for unknown_encoding in unknown_encodings:
+        results = face_recognition.compare_faces(known_faces, unknown_encoding)
+        for result in results:
+            if result:
+                isKnown = True
 
-if isKnown:
-    print("Known")
-else:
-    print("Unknown")
+    if isKnown:
+        print("Known")
+    else:
+        print("Unknown")

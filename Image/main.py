8a5680bda5487359b7cfe6feaad0ea4c35f3ba92
bhrotovy <bhrotovy@uwaterloo.ca>
2020-10-15 10:59:27 -0400

    Made a simple is Bohdan or is not Bohdan recognizer

---
+++ Image/main.py
@@ -0,0 +1,14 @@
+import face_recognition
+
+bohdan = face_recognition.load_image_file("image1.jpg")
+unknown = face_recognition.load_image_file("unknown.jpg")
+
+bohdan_encoding = face_recognition.face_encodings(bohdan)[0]
+unknown_encoding = face_recognition.face_encodings(unknown)[0]
+
+results = face_recognition.compare_faces([bohdan_encoding], unknown_encoding)
+
+if results[0]:
+    print("Bohdan")
+else:
+    print("Uknown")

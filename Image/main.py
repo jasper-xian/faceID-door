0ba5ff00c0282a90efad924c7e89fc62940c99be
bhrotovy <bhrotovy@uwaterloo.ca>
2020-10-15 11:56:42 -0400

    added support for multiple faces in one image

--- Image/main.py
+++ Image/main.py
@@ -8,18 +8,20 @@ for image in known_files:
     face = face_recognition.load_image_file(os.getcwd()+'/Known/'+image)
     known_faces.append(face_recognition.face_encodings(face)[0])
 
+# insert code to fetch uknown image from camera
 
-unknown = face_recognition.load_image_file("barack.jpg")
+# processes unknown image then removes it
+unknown = face_recognition.load_image_file("unknown.jpg")
+# os.remove("unknown.jpg")
 
-unknown_encoding = face_recognition.face_encodings(unknown)[0]
-
-results = face_recognition.compare_faces(known_faces, unknown_encoding)
+unknown_encodings = face_recognition.face_encodings(unknown)
 
 isKnown = False
-
-for result in results:
-    if result:
-        isKnown = True
+for unknown_encoding in unknown_encodings:
+    results = face_recognition.compare_faces(known_faces, unknown_encoding)
+    for result in results:
+        if result:
+            isKnown = True
 
 if isKnown:
     print("Known")

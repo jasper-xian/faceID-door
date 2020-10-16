5ed8ae2570d469847596f38b6b64f848b9dda11f
bhrotovy <bhrotovy@uwaterloo.ca>
2020-10-16 12:38:44 -0400

    added database person removal system

--- Image/main.py
+++ Image/main.py
@@ -13,19 +13,21 @@ drive = GoogleDrive(gauth)
 
 known_faces = []
 
+names = []
+
 known_files = os.listdir(os.getcwd()+'/Known')
 
 for image in known_files:
     face = face_recognition.load_image_file(os.getcwd()+'/Known/'+image)
     known_faces.append(face_recognition.face_encodings(face)[0])
+    names.append(image[0:-4])
 
 while True:
-    #add new faces from database to new folder
-
     # process new faces, then add them to Known folder
     file_list = drive.ListFile({'q': "'1B0MakdPdPHCq569YeJLbVx6fcCdBAXPi' in parents and trashed=false"}).GetList()
 
     for file in file_list:
+        names.append(file['title'][0:-4])
         file2 = drive.CreateFile({'id': file['id']})
         file2.GetContentFile(file['title'])
         face = face_recognition.load_image_file(file['title'])
@@ -33,10 +35,31 @@ while True:
         shutil.move(file['title'], os.getcwd() + '/Known/' + file['title'])
         file.Delete()
 
+    #remove faces on remove list
+    file3 = drive.CreateFile({'id': '1sm6gcDNQqzzRxwFYcAqZG_H8ssClwtyQ'})
+    file3.GetContentFile('remove.txt')
+    f = open('remove.txt', 'r')
+    x = f.readline()
+    while x != '':
+        if x[-1:] == '\n':
+            x = x[0:-1]
+        if x != "start":
+            os.remove(os.getcwd() + '/Known/' + x + ".jpg")
+            index = names.index(x)
+            known_faces.pop(index)
+            names.pop(index)
+        x = f.readline()
+    f.close()
+    w = open('remove.txt', 'w')
+    w.write("start")
+    w.close()
+    file3.SetContentFile('remove.txt')
+    file3.Upload()
+
     # insert code to fetch unknown image from camera
 
 
-    # processes image then changes removes it
+    # processes image then removes it
     unknown = face_recognition.load_image_file("unknown.jpg")
     os.remove("unknown.jpg")
     shutil.copyfile("placeholder.jpg","unknown.jpg")
--- Image/test.py
+++ Image/test.py
@@ -1,16 +1,23 @@
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
+import os
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
-
+# experimental code for removing people
 file = drive.CreateFile({'id':'1sm6gcDNQqzzRxwFYcAqZG_H8ssClwtyQ'})
 file.GetContentFile('remove.txt')
 f = open('remove.txt','r')
-print(f.read())
+x = f.readline()
+while x != '':
+    if x[-1:] == '\n':
+        x = x[0:-1]
+    if x != "start":
+        os.remove(os.getcwd() + '/Known/' + x +".jpg")
+    x = f.readline()
 f.close()
 w = open('remove.txt','w')
 w.write("start")
